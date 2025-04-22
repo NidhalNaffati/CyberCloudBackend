fe35701b241aaa254a0f5be112ee87dedf02ff64
ghazi <96641922+Ghazimabrouki@users.noreply.github.com>
2025-04-22 09:47:57 +0100

    Clear repository before new push

--- backend/gestion_forum/src/main/java/com/buzzsocial/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.buzzsocial.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
